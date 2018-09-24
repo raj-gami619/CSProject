6eb355279fbb41845de40974de71e060d5c29eea
Amani_06 <38758226+AmaniUsagi@users.noreply.github.com>
2018-09-24 12:27:06 +0300

    edit

--- dashboard.php
+++ dashboard.php
@@ -2,7 +2,7 @@
 <?php include 'head.php'; ?>
 <body>
 <?php include 'dashboard_navbar.php'; ?>
-<h3>Recent Additions</h3>
+<h3>Recent Activity</h3>
 <div class="table-responsive">
 	<table class="table table-bordered table-striped table-hover project_table">
 		<thead>
@@ -10,31 +10,32 @@
 				<th>Unit Name</th>
 				<th>Unit Code</th>
 				<th>Faculty</th>
-				<th>Unit_Level</th>
+				<th>Unit Level</th>
+				<th>Date Added</th>
 			</tr>
 		</thead>
 		<tbody>
-			<?php
-             $query = $db->query("SELECT * FROM project");
+		<?php
+           $query = $db->query("SELECT * FROM project");
              $rows = $query->fetchAll(PDO::FETCH_OBJ);
              foreach($rows as $row){
-               $project_name = $row->name;
-               $unit_code = $row->unit_code;
+			   $project_name = $row->project_name;
+			   $unit_code = $row->unit_code;
 			   $faculty = $row->faculty;
 			   $project_level = $row->project_level;
-               $id = $row->id;
-               $run = $db->query("SELECT project_name, FROM project WHERE unit_code = '$unit_code'");
+			   $date = $row->date;
+               $run = $db->query("SELECT project_name FROM project WHERE unit_code = '$unit_code'");
                $run_rows = $run->fetchAll(PDO::FETCH_OBJ);
                foreach($run_rows as $runrow){
-                //   $case = $runrow->project_case;
                   $title = $runrow->project_name;
                ?>
+			   
                <tr>
-               	<td><?php echo $unit_code; ?></td>
-               	<td><?php echo $name; ?></td>
-               	<td class="text-bold"><?php echo $title; ?></td>
-               	<td><?php echo $project_level; ?></td>
-               	<td><?php echo $faculty; ?></td>
+			   <td class="text-bold"><?php echo $project_name; ?></td>
+				<td><?php echo $unit_code; ?></td>
+				<td><?php echo $faculty; ?></td>
+             	<td><?php echo $project_level ?></td>
+				<td><?php echo $date; ?></td>
                </tr>
                <?php
                }
